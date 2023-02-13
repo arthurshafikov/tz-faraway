0f40ee7efb0ab9d914c69263685c3c3b91110647
arthurshafikov <arthur.shafikov27@gmail.com>
2023-02-13 14:30:58 +0400

    use powtcp lib in server

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,4 @@ module github.com/arthurshafikov/tz-faraway/server
 
 go 1.18
 
-require github.com/arthurshafikov/tz-faraway/lib v0.0.0-20230212083457-07dc147bee8f // indirect
+require github.com/arthurshafikov/tz-faraway/lib v0.0.0-20230213101526-5587f6dbf7f3 // indirect
--- server/internal/transport/http/server.go
+++ server/internal/transport/http/server.go
@@ -4,12 +4,14 @@ import (
 	"errors"
 	"log"
 	"net/http"
+
+	"github.com/arthurshafikov/tz-faraway/lib/powtcp"
 )
 
 func RunServer(handler http.Handler, port string) {
 	log.Println("Starting the server on port " + port)
 
-	proowOfWorkProtectionListener, err := NewProowOfWorkProtectionListener(Options{
+	proowOfWorkProtectionListener, err := powtcp.NewProowOfWorkProtectionListener(powtcp.Options{
 		Address: ":" + port,
 	})
 	if err != nil {
