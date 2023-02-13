b7f62d0da298244ce4b392c5b1e1f8fe5ae474d7
arthurshafikov <arthur.shafikov27@gmail.com>
2023-02-13 14:12:45 +0400

    move dialer to library

--- client/internal/client/client.go
+++ client/internal/client/client.go
@@ -1,16 +1,10 @@
 package client
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
 	"io"
 	"log"
-	"math"
-	"math/big"
-	"net"
 	"net/http"
-	"strconv"
 )
 
 func MakeQuery() {
@@ -42,72 +36,4 @@ func MakeQuery() {
 	defer res.Body.Close()
 
 	fmt.Printf("%s %s\n", res.Status, string(bodyRes))
-}
-
-type ConnDialer struct {
-	c net.Conn
-}
-
-func NewConnDialer(address string) (*ConnDialer, error) {
-	conn, err := net.Dial("tcp", address)
-	if err != nil {
-		return nil, err
-	}
-
-	return &ConnDialer{
-		c: conn,
-	}, nil
-}
-
-func (cd ConnDialer) CloseConnection() error {
-	return cd.c.Close()
-}
-
-func (cd ConnDialer) Dial(network, addr string) (net.Conn, error) {
-	buffer := make([]byte, 300)
-	n, err := cd.c.Read(buffer)
-	if err != nil {
-		if err != io.EOF {
-			return nil, fmt.Errorf("accept() read error: %w", err)
-		}
-	}
-
-	dataWithDifficulty := bytes.Split(buffer[:n], []byte(":"))
-	if len(dataWithDifficulty) != 2 {
-		return nil, fmt.Errorf("wrong data with difficulty came from host: %s", string(buffer[:n]))
-	}
-
-	data := dataWithDifficulty[0]
-	difficulty, err := strconv.Atoi(string(dataWithDifficulty[1]))
-	if err != nil || difficulty < 1 {
-		return nil, fmt.Errorf("wrong difficulty came from host: %s", string(dataWithDifficulty[1]))
-	}
-
-	nonce := findNonce(data, difficulty)
-	if _, err := cd.c.Write([]byte(fmt.Sprintf("%v", nonce))); err != nil {
-		return nil, fmt.Errorf("connection write error: %w", err)
-	}
-
-	return cd.c, nil
-}
-
-func findNonce(data []byte, difficulty int) int {
-	target := big.NewInt(1)
-	target.Lsh(target, uint(256-difficulty))
-
-	var hash [32]byte
-	var intHash big.Int
-	for nonce := 0; nonce < math.MaxInt64; nonce++ {
-		hash = sha256.Sum256(bytes.Join([][]byte{data, []byte(fmt.Sprintf("%v", nonce))}, []byte{}))
-
-		intHash.SetBytes(hash[:])
-		if intHash.Cmp(target) == -1 {
-			fmt.Printf("Nonce = %v\n", nonce)
-			fmt.Printf("Result = %s\n", intHash.String())
-			fmt.Printf("Target = %s\n", target.String())
-			return nonce
-		}
-	}
-
-	return 0
 }
